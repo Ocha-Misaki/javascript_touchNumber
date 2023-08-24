1475946047c52c917fbbf936cd180bd3ec39c1c1
Ocha-Misaki <himawari.158fuji@gmail.com>
2023-08-24 11:41:49 +0900

    panelクラスとboardクラスを作成

---
+++ dotinstall.js
@@ -0,0 +1,20 @@
+'use strict' 
+{
+  class Panel {
+    constructor(){
+      this.button = document.createElement('button')
+      this.button.className = 'inactive'
+    }
+  }
+  class Board {
+    constructor(){
+      this.pales = []
+      for(let i = 0; i < 4; i++){
+        this.pales.push(new Panel())
+      }
+    }
+  }
+
+  const board = new Board()
+  console.log(board)
+}
